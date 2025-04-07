de16194ee123102f41d23c3cc13fda9987a09d4e
halliez1 <baptiste.halliez@etu.unilim.fr>
2025-04-07 11:52:00 +0200

    aucune idée

--- server/model.php
+++ server/model.php
@@ -17,3 +17,24 @@ define("HOST", "localhost");
 define("DBNAME", "halliez1");
 define("DBLOGIN", "halliez1");
 define("DBPWD", "halliez1");
+
+/**
+ * Récupère le menu pour un jour spécifique dans la base de données.
+ *
+ * @param string $m Le film pour lequel le menu est récupéré.
+ * @param string $j Le jour pour lequel le menu est récupéré.
+ * @return array Un tableau d'objets contenant l'entrée, le plat principal et le dessert pour le jour spécifié.
+ */
+function getMovie($m){
+    // Connexion à la base de données
+    $cnx = new PDO("mysql:host=".HOST.";dbname=".DBNAME, DBLOGIN, DBPWD);
+    // Requête SQL pour récupérer le menu avec des paramètres
+    $sql = "select name from Movie";
+    // Prépare la requête SQL
+    $stmt = $cnx->prepare($sql);
+    // Exécute la requête SQL
+    $stmt->execute();
+    // Récupère les résultats de la requête sous forme d'objets
+    $res = $stmt->fetchAll(PDO::FETCH_OBJ);
+    return $res; // Retourne les résultats
+}
