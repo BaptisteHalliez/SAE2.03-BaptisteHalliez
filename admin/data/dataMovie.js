d6dbdaf4e4c7e502db97b5fa9c17f8ef2b1b4f3d
halliez1 <baptiste.halliez@etu.unilim.fr>
2025-04-10 17:02:15 +0200

    css app + iter 2 début

---
+++ admin/data/dataMovie.js
@@ -0,0 +1,34 @@
+
+// URL où se trouve le répertoire "server" sur mmi.unilim.fr
+let HOST_URL = "https://mmi.unilim.fr/~halliez1/SAE2.03-BaptisteHalliez/";
+
+let DataMovie = {};
+
+/**
+ *
+ * @param {string} day
+ * @param {string} week
+ * @returns The response from the server.
+ *
+ */
+DataMovie.request = async function () {
+  // fetch permet d'envoyer une requête HTTP à l'URL spécifiée.
+  let answer = await fetch(HOST_URL + "/server/script.php?todo=read");
+  let data = await answer.json();
+  return data;
+};
+
+/** DataMovie.add
+ * 
+ * @param {*} fdata un objet FormData contenant les données du formulaire à envoyer au serveur.
+ * @returns la réponse du serveur.
+ */
+DataMovie.add = async function (fdata) {
+    let config = {
+        method: "POST", // méthode HTTP à utiliser
+        body: fdata // données à envoyer sous forme d'objet FormData
+    };
+    let answer = await fetch(HOST_URL + "/server/script.php?todo=addMovie", config);
+    let data = await answer.json();
+    return data;
+}
